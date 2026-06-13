e5c1f359df3406ba6bea273caf2cdb25f7c0401e
ricoroyadm <ricoroy.adm@gmail.com>
2026-06-13 18:44:16 +0700

    Index ujian + utss

--- js/utss.js
+++ js/utss.js
@@ -7,6 +7,15 @@ const links = {
   MS: "https://forms.gle/PsszuK39RRZVWyh77",
   PKns: "https://forms.gle/Xh58zHRGMg1Cd7ji7",
   MIP: "https://forms.gle/tNAEyzYNeSCTb71w9",
+  PAg: "https://forms.gle/iWu6jBgiopApaMPU6",
+  BIng2: "https://forms.gle/PstG9nJSBfPBgKd5A",
+  PKn: "https://forms.gle/NFFT5FAGYPd6X5LC8",
+  MK1: "https://forms.gle/du5gtEdAMyEXqPcL9",
+  ISD: "https://forms.gle/ekvhggrfweyZydMr9",
+  EM: "https://forms.gle/UusosYUnn2fLGwpo6",
+  PPSDM: "https://forms.gle/4hjWJcAMTbYL5Kgy5",
+  KssMnj: "https://forms.gle/LXZNCdi1Qnd5ELuG9",
+  PPO: "https://forms.gle/jBtyVvaotEk1CuXo6",
 };
 
 // 2. Ambil semua elemen button
@@ -17,6 +26,15 @@ const btnPjk = document.getElementById("btnPjk");
 const btnMS = document.getElementById("btnMS");
 const btnPKns = document.getElementById("btnPKns");
 const btnMIP = document.getElementById("btnMIP");
+const btnPAg = document.getElementById("btnPAg");
+const btnBIng2 = document.getElementById("btnBIng2");
+const btnPKn = document.getElementById("btnPKn");
+const btnMK1 = document.getElementById("btnMK1");
+const btnISD = document.getElementById("btnISD");
+const btnEM = document.getElementById("btnEM");
+const btnPPSDM = document.getElementById("btnPPSDM");
+const btnKssMnj = document.getElementById("btnKssMnj");
+const btnPPO = document.getElementById("btnPPO");
 
 // 3. Pasang fungsi klik untuk masing-masing tombol
 btnBIng1.addEventListener("click", () => {
@@ -45,4 +63,36 @@ btnPKns.addEventListener("click", () => {
 
 btnMIP.addEventListener("click", () => {
   window.open(links.MIP, "_blank");
+});
+
+btnPAg.addEventListener("click", () => {
+  window.open(links.PAg, "_blank");
+});
+
+btnBIng2.addEventListener("click", () => {
+  window.open(links.BIng2, "_blank");
+});
+
+btnPKn.addEventListener("click", () => {
+  window.open(links.PKn, "_blank");
+});
+
+btnMK1.addEventListener("click", () => {
+  window.open(links.MK1, "_blank");
+});
+
+btnISD.addEventListener("click", () => {
+  window.open(links.ISD, "_blank");
+});
+
+btnEM.addEventListener("click", () => {
+  window.open(links.EM, "_blank");
+});
+
+btnPPSDM.addEventListener("click", () => {
+  window.open(links.PPSDM, "_blank");
+});
+
+btnPPO.addEventListener("click", () => {
+  window.open(links.PPO, "_blank");
 });
