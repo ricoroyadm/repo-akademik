aad143bbef3a84db429cabc8ebd87ea3b2f14682
ricoroyadm <ricoroy.adm@gmail.com>
2026-07-21 11:08:04 +0700

    Update jurnal.js
    add link G-Drive Pak Syamsul

--- js/jurnal.js
+++ js/jurnal.js
@@ -9,6 +9,7 @@
 // 1. Kumpulan URL dalam satu variabel objek
 const links = {
   ade: "https://drive.google.com/drive/folders/1RNS-i--GUMTY4HflYYkgRoONAQoJa2bi?usp=drive_link",
+  syam: "https://drive.google.com/drive/folders/1YQ_-E4HGqb_zfUj301beFTeXbEv25vME?usp=drive_link",
   eka: "https://drive.google.com/drive/folders/1z8hD_YWJiGR_gGrXaFelQm0IQgQSezDD?usp=drive_link",
   lis: "https://drive.google.com/drive/folders/1kGUTYmyiyiHKfnFtx1iZLPaPGTqXnFhl?usp=drive_link",
   lisM: "https://drive.google.com/drive/folders/12Uv7sLzKG-DQK_kyTsbihsE95stn1Z9P?usp=drive_link",
@@ -53,6 +54,7 @@ const links = {
 
 // 2. Ambil semua elemen button
 const btnAde = document.getElementById("btnAde");
+const btnSyam = document.getElementById("btnSyam");
 const btnEka = document.getElementById("btnEka");
 const btnLis = document.getElementById("btnLis");
 const btnLisM = document.getElementById("btnLisM");
@@ -97,6 +99,10 @@ const btnSyamM = document.getElementById("btnSyamM");
 // 3. Pasang fungsi klik untuk masing-masing tombol
 btnAde.addEventListener("click", () => {
   window.open(links.ade, "_blank");
+});
+
+btnSyam.addEventListener("click", () => {
+  window.open(links.syam, "_blank");
 });
 
 btnEka.addEventListener("click", () => {
